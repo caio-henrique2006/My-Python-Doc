75ffc69e8ecd6af02ff2d7455435c6b8143a5b3a
caio-henrique2006 <74103264+caio-henrique2006@users.noreply.github.com>
2022-02-23 09:58:21 -0300

    justChillling v1.01

--- justChilling.py
+++ justChilling.py
@@ -1,1 +1,4 @@
-print("Hey bruh, how you doing?")
+name = "Caio Henrique"
+age = 15
+tastes = ["play video games", "read", "watch anime"]
+print("Greetings, I'm ", name,"i have", age,"and i like to", tastes[1])
